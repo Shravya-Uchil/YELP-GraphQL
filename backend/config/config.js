73131a73cae9fab2ea45c982018c73377b5a3e0a
Shravya Uchil <57883123+Shravya-Uchil@users.noreply.github.com>
2020-12-29 16:28:16 -0800

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ const config = {
   secret: 'yelp_graphql_mongo',
   frontendURI: 'http://localhost:3000',
   mongoDBURI:
-    'mongodb+srv://mongoDB:mongopwd123@yelp-mongodb.vhz3e.mongodb.net/yelp?retryWrites=true&w=majority',
+    '',
 };
 
 module.exports = config;
